63937ddbb638b131f4388a3dc7c406257591db39
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2023-03-18 21:59:33 +0300

    Added change language function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 HOST = '127.0.0.1'
@@ -6,12 +6,28 @@ HOST = '127.0.0.1'
 
 @app.route('/')
 def main_web_page():
-    return render_template('MainWebPage.html', host_url='http://127.0.0.1:5000')
+    return redirect("http://127.0.0.1:5000/ru")
 
 
-@app.route('/aboba')
-def aboba_page():
-    return render_template('aboba.html', host_url='http://127.0.0.1:5000')
+@app.route('/en')
+def main_web_page_en():
+    return render_template('main_page_en.html', host_url='http://127.0.0.1:5000/en', language='en', domen='')
+
+
+@app.route('/ru')
+def main_web_page_ru():
+    return render_template('main_page_ru.html', host_url='http://127.0.0.1:5000/ru', language='ru', domen='')
+
+
+@app.route('/<language>/aboba')
+def aboba_page(language):
+    return render_template(f'aboba_{language}.html', host_url=f'http://127.0.0.1:5000/{language}', domen='/aboba',
+                           language=language)
+
+
+@app.route('/switch_language/<new_lang>')
+def switch_language(language):
+    pass
 
 
 if __name__ == '__main__':
