38d85bc63dbdae0263193c649110e4180a30f44b
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2023-03-18 17:30:48 +0300

    Server bugs fix

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+HOST = '127.0.0.1'
 
 
 @app.route('/')
 def main_web_page():
-    return render_template('MainWebPage.html')
+    return render_template('MainWebPage.html', host_url='http://127.0.0.1:5000')
 
 
 @app.route('/aboba')
 def aboba_page():
-    return render_template('aboba.html')
+    return render_template('aboba.html', host_url='http://127.0.0.1:5000')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run(host=HOST)
